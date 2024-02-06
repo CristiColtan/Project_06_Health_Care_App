c421e00052090e851298cf88a9d6efe3d0f0abc7
CristiColtan <118475284+CristiColtan@users.noreply.github.com>
2024-02-06 17:11:56 +0200

    Add files via upload

---
+++ HealthCare/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HealthCare.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl, INotifyPropertyChanged
+    {
+        public ClearableTextBox()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+        private string placeholder;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                //do not!
+                //tbPlaceholder.Text = placeholder;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Username"));
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+                tbPlaceholder.Visibility = Visibility.Hidden;
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+    }
+}
